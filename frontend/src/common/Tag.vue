96731524d46c70291a7c2cefca667949e62d65f3
Leo Torres <leo.torres@fgsglobal.com>
2025-04-02 23:46:28 +0200

    show tags

---
+++ frontend/src/common/Tag.vue
@@ -0,0 +1,23 @@
+<script setup>
+ import { ref } from 'vue';
+
+ const props = defineProps({
+     name: { type: String, required: true }
+ })
+
+</script>
+
+
+<template>
+  <span class="pill">{{ name }}</span>
+</template>
+
+
+<style scoped>
+ .pill {
+     border-radius: 8px;
+     background-color: var(--secondary-100);
+     padding-inline: 8px;
+     padding-block: 4px;
+ }
+</style>
