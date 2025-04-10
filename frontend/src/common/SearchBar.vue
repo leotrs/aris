a88e248e0de362346039cd006ae7e0cc1c5f8147
Leo Torres <leo@leotrs.com>
2025-04-10 21:28:23 +0200

    prettier, css, and start development of search function

--- frontend/src/common/SearchBar.vue
+++ frontend/src/common/SearchBar.vue
@@ -1,36 +1,54 @@
 <script setup>
- import { IconSearch } from '@tabler/icons-vue';
+import { ref, inject } from 'vue';
+import { IconSearch } from '@tabler/icons-vue';
+
+const searchText = ref("");
+const { userDocs, filterDocs, clearFilterDocs } = inject('userDocs');
+
+const submit = () => {
+  console.log('submit');
+  clearFilterDocs();
+  filterDocs((doc) => !doc.title.toLowerCase().includes(searchText.value.toLowerCase()));
+}
 </script>
 
 
 <template>
-  <div class="search-input text-caption">
-    <IconSearch />Search...
+  <div class="s-wrapper text-caption">
+    <IconSearch />
+    <input
+      type="text"
+      placeholder="Search..."
+      v-model="searchText"
+      @keyup.enter="submit"
+      @keyup.ESC="(searchText = '') && submit()"
+      @click.stop
+      @dblclick.stop />
   </div>
 </template>
 
 
 <style scoped>
- .search-input {
-     color: var(--extra-dark);
-     background-color: var(--surface-primary);
-     border: 2px solid var(--border-primary);
-     border-radius: 16px;
-     height: 48px;
-     min-width: 280;
-     display: flex;
-     align-items: center;
-     padding-block: 12px;
-     padding-inline: 16px;
-     gap: 16px;
+.s-wrapper {
+  color: var(--extra-dark);
+  background-color: var(--surface-primary);
+  border: 2px solid var(--border-primary);
+  border-radius: 16px;
+  height: 48px;
+  min-width: 280;
+  display: flex;
+  align-items: center;
+  padding-block: 12px;
+  padding-inline: 16px;
+  gap: 16px;
 
-     & > .tabler-icon {
-         margin: unset;
-     }
+  & > .tabler-icon {
+    margin: unset;
+  }
 
-     &:hover {
-         cursor: text;
-     }
+  &:hover {
+    cursor: text;
+  }
 
- }
+}
 </style>
--- frontend/src/home/HomeView.vue
+++ frontend/src/home/HomeView.vue
@@ -1,149 +1,152 @@
 <script setup>
- import { ref, provide, computed, useTemplateRef, onMounted, onUnmounted } from 'vue';
- import { useDraggable } from '@vueuse/core'
- import axios from 'axios';
- import Sidebar from './Sidebar.vue';
- import Topbar from './Topbar.vue';
- import DocumentsPane from './DocumentsPane.vue';
- import PreviewPane from './PreviewPane.vue';
- import UploadFileModal from './UploadFileModal.vue';
- import Separator from '../common/Separator.vue';
+import { ref, provide, computed, useTemplateRef, onMounted, onUnmounted } from 'vue';
+import { useDraggable } from '@vueuse/core'
+import axios from 'axios';
+import Sidebar from './Sidebar.vue';
+import Topbar from './Topbar.vue';
+import DocumentsPane from './DocumentsPane.vue';
+import PreviewPane from './PreviewPane.vue';
+import UploadFileModal from './UploadFileModal.vue';
+import Separator from '../common/Separator.vue';
 
- const showModal = ref(false);
- const currentMode = ref("list");
- const selfRef = useTemplateRef('selfRef');
+const showModal = ref(false);
+const currentMode = ref("list");
+const selfRef = useTemplateRef('selfRef');
 
- /*********** userID ***********/
- const userID = 1;
- provide('userID', userID);
+/*********** userID ***********/
+const userID = 1;
+provide('userID', userID);
 
- /*********** isMobile ***********/
- const isMobile = ref(false);
- const setIsMobile = (el) => {
-     if (el?.contentRect) {
-         isMobile.value = el.contentRect.width < 432;
-     }
- }
- let observer;
- onMounted(() => {
-     if (selfRef.value) {
-         observer = new ResizeObserver((entries) => {
-             for (let entry of entries) {
-                 setIsMobile(entry);
-             }
-         });
-         observer.observe(selfRef.value);
-         setIsMobile(selfRef.value);
-     }
- })
- onUnmounted(() => { if (observer) observer.disconnect() });
- provide('isMobile', isMobile);
+/*********** isMobile ***********/
+const isMobile = ref(false);
+const setIsMobile = (el) => {
+  if (el?.contentRect) {
+    isMobile.value = el.contentRect.width < 432;
+  }
+}
+let observer;
+onMounted(() => {
+  if (selfRef.value) {
+    observer = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        setIsMobile(entry);
+      }
+    });
+    observer.observe(selfRef.value);
+    setIsMobile(selfRef.value);
+  }
+})
+onUnmounted(() => { if (observer) observer.disconnect() });
+provide('isMobile', isMobile);
 
- /*********** userDocs ***********/
- const userDocs = ref([]);
- const reloadDocs = async (docID) => {
-     try {
-         const response = await axios.get(`http://localhost:8000/users/${userID}/documents`);
-         if (!userDocs.value) {
-             userDocs.value = response.data.map(doc => ({ ...doc, filtered: false }));
-         } else {
-             /* FIX ME: take the filtered value from the current userDocs, not from response.data */
-             userDocs.value = response.data.map(doc => ({ ...doc, filtered: false }));
-         }
-     } catch (error) {
-         console.error(`Failed to fetch document`, error);
-     }
- };
- const sortDocs = async (func) => {
-    userDocs.value.sort((a, b) => func(a, b));
- };
- const filterDocs = async (docID) => {
+/*********** userDocs ***********/
+const userDocs = ref([]);
+const reloadDocs = async (docID) => {
+  try {
+    const response = await axios.get(`http://localhost:8000/users/${userID}/documents`);
+    if (!userDocs.value) {
+      userDocs.value = response.data.map(doc => ({ ...doc, filtered: false }));
+    } else {
+      /* FIX ME: take the filtered value from the current userDocs, not from response.data */
+      userDocs.value = response.data.map(doc => ({ ...doc, filtered: false }));
+    }
+  } catch (error) {
+    console.error(`Failed to fetch document`, error);
+  }
+};
+const sortDocs = async (func) => {
+  userDocs.value.sort((a, b) => func(a, b));
+};
+const filterDocs = async (func) => {
+  userDocs.value = userDocs.value.map(doc => ({...doc, filtered: func(doc)}))
+};
+const clearFilterDocs = async () => {
+  filterDocs(doc => false);
+};
+provide('userDocs', { userDocs, reloadDocs, sortDocs, filterDocs, clearFilterDocs });
+onMounted(async () => { reloadDocs() });
 
- };
- provide('userDocs', { userDocs, reloadDocs, sortDocs, filterDocs });
- onMounted(async () => { reloadDocs() });
+/*********** userTags ***********/
+const userTags = ref([]);
+const updateUserTag = async (oldTag, newTag) => {
+  if (oldTag) {
+    const url = `http://localhost:8000/users/${userID}/tags/${oldTag.id}`;
+    try {
+      if (newTag == null) { await axios.delete(url) }
+      else { await axios.put(url, newTag) }
+      reloadDocs();
+    } catch (error) {
+      console.error('Error updating tag:', error);
+    }
+  }
 
- /*********** userTags ***********/
- const userTags = ref([]);
- const updateUserTag = async (oldTag, newTag) => {
-     if (oldTag) {
-         const url = `http://localhost:8000/users/${userID}/tags/${oldTag.id}`;
-         try {
-             if (newTag == null) { await axios.delete(url) }
-             else { await axios.put(url, newTag) }
-             reloadDocs();
-         } catch (error) {
-             console.error('Error updating tag:', error);
-         }
-     }
+  try {
+    const response = await axios.get(`http://localhost:8000/users/${userID}/tags`);
+    userTags.value = response.data;
+  } catch (error) {
+    console.error('Failed to fetch tags:', error);
+  }
 
-     try {
-         const response = await axios.get(`http://localhost:8000/users/${userID}/tags`);
-         userTags.value = response.data;
-     } catch (error) {
-         console.error('Failed to fetch tags:', error);
-     }
+};
+const createTag = async (name, color=null) => {
+  try {
+    const response = await axios.post(
+      `http://localhost:8000/users/${userID}/tags`,
+      { name: name, color: color || ''}
+    );
+    reloadDocs();
+  } catch (error) {
+    console.error('Error creating tag:', error);
+  }
+}
+const addOrRemoveTag = async (tagID, docID, mode) => {
+  const url = `http://localhost:8000/users/${userID}/documents/${docID}/tags/${tagID}`;
+  if (mode == 'add') {
+    try {
+      await axios.post(url);
+      reloadDocs();
+    } catch (error) {
+      console.error('Error updating tag:', error);
+    }
+  } else if (mode == 'remove') {
+    try {
+      await axios.delete(url);
+      reloadDocs();
+    } catch (error) {
+      console.error('Error updating tag:', error);
+    }
+  }
+}
+provide('userTags', { userTags, updateUserTag, createTag, addOrRemoveTag });
+onMounted(async () => { updateUserTag() });
 
- };
- const createTag = async (name, color=null) => {
-     try {
-         const response = await axios.post(
-             `http://localhost:8000/users/${userID}/tags`,
-             { name: name, color: color || ''}
-         );
-         reloadDocs();
-     } catch (error) {
-         console.error('Error creating tag:', error);
-     }
- }
- const addOrRemoveTag = async (tagID, docID, mode) => {
-     const url = `http://localhost:8000/users/${userID}/documents/${docID}/tags/${tagID}`;
-     if (mode == 'add') {
-         try {
-             await axios.post(url);
-             reloadDocs();
-         } catch (error) {
-             console.error('Error updating tag:', error);
-         }
-     } else if (mode == 'remove') {
-         try {
-             await axios.delete(url);
-             reloadDocs();
-         } catch (error) {
-             console.error('Error updating tag:', error);
-         }
-     }
- }
- provide('userTags', { userTags, updateUserTag, createTag, addOrRemoveTag });
- onMounted(async () => { updateUserTag() });
+/*********** draggable Preview pane ***********/
+const container = useTemplateRef('separator-container-ref');;
+const separator = useTemplateRef('separator-ref');
+const preview = useTemplateRef('preview-ref');
 
- /*********** draggable Preview pane ***********/
- const container = useTemplateRef('separator-container-ref');;
- const separator = useTemplateRef('separator-ref');
- const preview = useTemplateRef('preview-ref');
-
- const previewHeight = ref('50%');
- const onSeparatorDragged = (pos, ev) => {
-     const rect = container.value.getBoundingClientRect();
-     console.log(pos.y, rect.height);
-     previewHeight.value = `calc(30% + ${rect.height}px - ${pos.y}px)`;
- }
- const style = ref({});
- const selectedForPreview = ref(null);
- const separatorPointerEvents = computed(() => selectedForPreview.value ? 'all' : 'none' );
- const setSelectedForPreview = (doc) => {
-     selectedForPreview.value = doc;
-     if (!doc) return;
-     if (!container.value) return;
-     const { style } = useDraggable(separator, {
-         initialValue: { x: 0, y: container.value.getBoundingClientRect().height / 2 - 2 },
-         preventDefault: true,
-         axis: 'y',
-         onMove: onSeparatorDragged,
-         containerElement: container,
-     });
-     return style;
- };
+const previewHeight = ref('50%');
+const onSeparatorDragged = (pos, ev) => {
+  const rect = container.value.getBoundingClientRect();
+  console.log(pos.y, rect.height);
+  previewHeight.value = `calc(30% + ${rect.height}px - ${pos.y}px)`;
+}
+const style = ref({});
+const selectedForPreview = ref(null);
+const separatorPointerEvents = computed(() => selectedForPreview.value ? 'all' : 'none' );
+const setSelectedForPreview = (doc) => {
+  selectedForPreview.value = doc;
+  if (!doc) return;
+  if (!container.value) return;
+  const { style } = useDraggable(separator, {
+    initialValue: { x: 0, y: container.value.getBoundingClientRect().height / 2 - 2 },
+    preventDefault: true,
+    axis: 'y',
+    onMove: onSeparatorDragged,
+    containerElement: container,
+  });
+  return style;
+};
 </script>
 
 
@@ -154,11 +157,11 @@
 
       <div id="documents" class="pane">
         <Topbar
-            @list="currentMode = 'list'"
-            @cards="currentMode = 'cards'" />
+          @list="currentMode = 'list'"
+          @cards="currentMode = 'cards'" />
         <DocumentsPane
-            @set-selected="(doc) => style = setSelectedForPreview(doc)"
-            :mode="currentMode" />
+          @set-selected="(doc) => style = setSelectedForPreview(doc)"
+          :mode="currentMode" />
       </div>
 
       <div class="separator-container" ref="separator-container-ref">
@@ -166,11 +169,11 @@
       </div>
 
       <PreviewPane
-          v-if="!isMobile && selectedForPreview"
-          ref="preview-ref"
-          :doc="selectedForPreview"
-          :container="useTemplateRef('panes-ref')"
-          @set-selected="(doc) => style = setSelectedForPreview(doc)" />
+        v-if="!isMobile && selectedForPreview"
+        ref="preview-ref"
+        :doc="selectedForPreview"
+        :container="useTemplateRef('panes-ref')"
+        @set-selected="(doc) => style = setSelectedForPreview(doc)" />
 
     </div>
     <div class="modal" v-show="showModal">
@@ -181,91 +184,91 @@
 
 
 <style scoped>
- .view-wrapper {
-     position: relative;
-     display: flex;
-     flex-grow: 2;
-     padding: 16px 16px 16px 0;
-     height: 100%;
- }
- 
- .view-wrapper.mobile {
-     padding: 0;
- }
+.view-wrapper {
+  position: relative;
+  display: flex;
+  flex-grow: 2;
+  padding: 16px 16px 16px 0;
+  height: 100%;
+}
 
- .separator-container {
-     height: 40%;
-     position: absolute;
-     width: 100%;
-     top: 30%;
-     pointer-events: none;
- }
+.view-wrapper.mobile {
+  padding: 0;
+}
 
- .separator {
-     position: absolute;
-     bottom: 0;
-     background-color: transparent;
-     width: 100%;
-     height: 12px;
-     z-index: 1;
-     pointer-events: v-bind(separatorPointerEvents);
-     &:hover{ cursor: row-resize }
- }
+.separator-container {
+  height: 40%;
+  position: absolute;
+  width: 100%;
+  top: 30%;
+  pointer-events: none;
+}
 
- .panes {
-     position: relative;
-     flex-grow: 1;
-     height: 100%;
+.separator {
+  position: absolute;
+  bottom: 0;
+  background-color: transparent;
+  width: 100%;
+  height: 12px;
+  z-index: 1;
+  pointer-events: v-bind(separatorPointerEvents);
+  &:hover{ cursor: row-resize }
+}
 
-     & :deep(.pane) {
-         background-color: var(--almost-white);
-         padding: 16px;
-         width: 100%;
-         display: flex;
-         flex-direction: column;
-         gap: 16px;
-         border-radius: 16px;
-     }
+.panes {
+  position: relative;
+  flex-grow: 1;
+  height: 100%;
 
-     & :deep(#documents:not(:has(~ .pane))) {
-         min-height: 100%;
-         max-height: 100%;
-     }
-     & :deep(#documents:has(~ .pane)) {
-         min-height: 20%;
-         max-height: 80%;
-     }
-     & :deep(#preview) {
-         min-height: 20%;
-         max-height: 80%;
-     }
- }
+  & :deep(.pane) {
+    background-color: var(--almost-white);
+    padding: 16px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    border-radius: 16px;
+  }
 
- .view-wrapper.mobile {
-     & :deep(.pane) {
-         padding: 0;
-         width: 100%;
-         display: flex;
-         flex-direction: column;
-         gap: 16px;
-         border-radius: 16px;
-     }
- }
- 
+  & :deep(#documents:not(:has(~ .pane))) {
+    min-height: 100%;
+    max-height: 100%;
+  }
+  & :deep(#documents:has(~ .pane)) {
+    min-height: 20%;
+    max-height: 80%;
+  }
+  & :deep(#preview) {
+    min-height: 20%;
+    max-height: 80%;
+  }
+}
 
- #documents {
-     margin-bottom: 8px;
-     height: calc(100% - v-bind(previewHeight));
- }
+.view-wrapper.mobile {
+  & :deep(.pane) {
+    padding: 0;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    border-radius: 16px;
+  }
+}
 
- #preview {
-     height: v-bind(previewHeight);
- }
 
- .modal {
-     position: absolute;
-     width: 100vw;
-     height: 100vh;
-     backdrop-filter: blur(2px) brightness(0.9);
- }
+#documents {
+  margin-bottom: 8px;
+  height: calc(100% - v-bind(previewHeight));
+}
+
+#preview {
+  height: v-bind(previewHeight);
+}
+
+.modal {
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  backdrop-filter: blur(2px) brightness(0.9);
+}
 </style>
