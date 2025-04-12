0774ec5c85323131c32f2050ac3df8f94a00ead3
Leo Torres <leo.torres@fgsglobal.com>
2025-04-12 15:43:39 +0200

    composable to register components and manage them globally; and some prettier stuff

--- frontend/src/home/UploadFileModal.vue
+++ frontend/src/home/UploadFileModal.vue
@@ -1,47 +1,53 @@
 <script setup>
- import { ref, onMounted } from 'vue';
- import { IconUpload, IconX } from '@tabler/icons-vue';
- import useClosable from '@/composables/useClosable.js';
+import { ref, onMounted } from "vue";
+import { IconUpload, IconX } from "@tabler/icons-vue";
+import useClosable from "@/composables/useClosable.js";
 
- const emit = defineEmits(["close"]);
- const close = () => { emit('close') };
- const selfRef = ref(null);
- onMounted(() => {
-     if (!selfRef) return;
-     useClosable(close, selfRef, true, true);
- })
+const emit = defineEmits(["close"]);
+const close = () => {
+  emit("close");
+};
+const selfRef = ref(null);
+onMounted(() => {
+  if (!selfRef) return;
+  /* useClosable(close, selfRef, true, true); */
+});
 
- const fileUpload = ref(null);
- const triggerFileUpload = () => { fileUpload.value?.click() };
+const fileUpload = ref(null);
+const triggerFileUpload = () => {
+  fileUpload.value?.click();
+};
 
- const upload = () => {
-     if (fileUpload.value.files.length == 0) return;
-     const file = fileUpload.value.files[0];
+const upload = () => {
+  if (fileUpload.value.files.length == 0) return;
+  const file = fileUpload.value.files[0];
 
-     const reader = new FileReader();
-     reader.onerror = () => { console.log("Error reading the file. Please try again.") };
-     reader.onload = () => {
-         const url = "http://localhost:8000/documents/";
-         console.log(reader.result);
-         fetch(url, {
-             method: 'POST',
-             headers: {
-                 'Content-Type': 'application/json'
-             },
-             body: JSON.stringify({
-                 source: reader.result,
-                 owner_id: 1,
-                 title: '',
-                 abstract: ''
-         })})
-             .then(response => response.json())
-             .then(data => console.log(data))
-             .catch(error => console.error('Error:', error));
-     };
-     reader.readAsText(file);
- }
+  const reader = new FileReader();
+  reader.onerror = () => {
+    console.log("Error reading the file. Please try again.");
+  };
+  reader.onload = () => {
+    const url = "http://localhost:8000/documents/";
+    console.log(reader.result);
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        source: reader.result,
+        owner_id: 1,
+        title: "",
+        abstract: "",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
+  };
+  reader.readAsText(file);
+};
 </script>
-
 
 <template>
   <div class="md-wrapper" ref="selfRef">
@@ -51,15 +57,16 @@
     </div>
     <div class="md-content">
       <span>Select a .rsm file from your computer:</span>
-      <input type="file" id="file-upload" ref="fileUpload" hidden>
+      <input type="file" id="file-upload" ref="fileUpload" hidden />
       <label for="file-upload">
         <Button
-            id="file-upload-cta"
-            kind="secondary"
-            text="Choose file"
-            icon="Upload"
-            class="btn-md"
-            @click="triggerFileUpload" />
+          id="file-upload-cta"
+          kind="secondary"
+          text="Choose file"
+          icon="Upload"
+          class="btn-md"
+          @click="triggerFileUpload"
+        />
         <span class="file-upload-echo text-caption">No file chosen.</span>
       </label>
     </div>
@@ -70,48 +77,47 @@
   </div>
 </template>
 
-
 <style scoped>
- .md-wrapper {
-     position: absolute;
-     left: 50%;
-     top: 50%;
-     transform: translateX(-50%) translateY(-50%);
-     border: 2px solid var(--border-information);
-     border-radius: 16px;
-     padding: 16px;
-     gap: 16px;
-     display: flex;
-     flex-direction: column;
-     min-width: 500px;
-     background-color: var(--surface-primary);
- }
+.md-wrapper {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  border: 2px solid var(--border-information);
+  border-radius: 16px;
+  padding: 16px;
+  gap: 16px;
+  display: flex;
+  flex-direction: column;
+  min-width: 500px;
+  background-color: var(--surface-primary);
+}
 
- .md-header {
-     display: flex;
-     justify-content: space-between;
-     align-items: center;
-     background-color: var(--surface-information);
-     padding-inline: 8px;
-     padding-block: 4px;
-     border-radius: 8px;
- }
+.md-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--surface-information);
+  padding-inline: 8px;
+  padding-block: 4px;
+  border-radius: 8px;
+}
 
- .md-content {
-     display: flex;
-     flex-direction: column;
-     gap: 8px;
- }
+.md-content {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
 
- .md-footer {
-     display: flex;
-     gap: 8px;
-     justify-content: flex-end;
- }
+.md-footer {
+  display: flex;
+  gap: 8px;
+  justify-content: flex-end;
+}
 
- #file-upload-cta {
-     width: 100%;
-     display: flex;
-     justify-content: center;
- }
+#file-upload-cta {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
 </style>
