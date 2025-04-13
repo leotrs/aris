c9816c9febc437cfc5ca8fcdf351c583dc098a58
Leo Torres <leo.torres@fgsglobal.com>
2025-04-13 17:55:39 +0200

    move userDocs to the documentsPane

--- frontend/src/home/DocumentsPane.vue
+++ frontend/src/home/DocumentsPane.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { ref, inject, computed } from "vue";
+import { ref, inject, provide, computed, onMounted } from "vue";
 import { useRouter } from "vue-router";
 import { useKeyboardShortcuts } from "@/composables/useKeyboardShortcuts.js";
+import axios from "axios";
 import Topbar from "./Topbar.vue";
 import DocumentsPaneHeader from "./DocumentsPaneHeader.vue";
 import DocumentsPaneItem from "./DocumentsPaneItem.vue";
@@ -10,9 +11,95 @@ const props = defineProps({
   mode: { type: String, default: "list" },
 });
 const emit = defineEmits(["set-selected"]);
-const { userDocs } = inject("userDocs");
 const numDocs = computed(() => userDocs.value.length);
+const userID = inject("userID");
 
+/*********** Proivde userDocs ***********/
+const userDocs = ref([]);
+const reloadDocs = async (docID) => {
+  try {
+    const response = await axios.get(`http://localhost:8000/users/${userID}/documents`);
+    if (!userDocs.value) {
+      userDocs.value = response.data.map((doc) => ({ ...doc, filtered: false }));
+    } else {
+      /* FIX ME: take the filtered value from the current userDocs, not from response.data */
+      userDocs.value = response.data.map((doc) => ({ ...doc, filtered: false }));
+    }
+  } catch (error) {
+    console.error(`Failed to fetch document`, error);
+  }
+};
+const sortDocs = async (func) => {
+  userDocs.value.sort((a, b) => func(a, b));
+};
+const filterDocs = async (func) => {
+  userDocs.value = userDocs.value.map((doc) => ({ ...doc, filtered: func(doc) }));
+};
+const clearFilterDocs = async () => {
+  filterDocs((_) => false);
+};
+provide("userDocs", { userDocs, reloadDocs, sortDocs, filterDocs, clearFilterDocs });
+onMounted(async () => reloadDocs());
+
+/*********** Provide userTags ***********/
+const userTags = ref([]);
+const updateUserTag = async (oldTag, newTag) => {
+  if (oldTag) {
+    const url = `http://localhost:8000/users/${userID}/tags/${oldTag.id}`;
+    try {
+      if (newTag == null) {
+        await axios.delete(url);
+      } else {
+        await axios.put(url, newTag);
+      }
+      reloadDocs();
+    } catch (error) {
+      console.error("Error updating tag:", error);
+    }
+  }
+
+  try {
+    const response = await axios.get(`http://localhost:8000/users/${userID}/tags`);
+    userTags.value = response.data;
+  } catch (error) {
+    console.error("Failed to fetch tags:", error);
+  }
+};
+const createTag = async (name, color = null) => {
+  try {
+    await axios.post(`http://localhost:8000/users/${userID}/tags`, {
+      name: name,
+      color: color || "",
+    });
+    reloadDocs();
+  } catch (error) {
+    console.error("Error creating tag:", error);
+  }
+};
+const addOrRemoveTag = async (tagID, docID, mode) => {
+  const url = `http://localhost:8000/users/${userID}/documents/${docID}/tags/${tagID}`;
+  if (mode == "add") {
+    try {
+      await axios.post(url);
+      reloadDocs();
+    } catch (error) {
+      console.error("Error updating tag:", error);
+    }
+  } else if (mode == "remove") {
+    try {
+      await axios.delete(url);
+      reloadDocs();
+    } catch (error) {
+      console.error("Error updating tag:", error);
+    }
+  }
+};
+provide("userTags", { userTags, updateUserTag, createTag, addOrRemoveTag });
+onMounted(async () => {
+  updateUserTag();
+});
+
+/* Handlers for child component events */
 const activeIndex = ref(null);
 let clickTimeout = ref(null);
 const selectForPreview = (doc, idx) => {
@@ -26,6 +113,7 @@ const openRead = (doc) => {
   router.push(`/${doc.id}/read`);
 };
 
+/* Keyboard shortcuts */
 const nextItemOnKey = (ev) => {
   ev.preventDefault();
   activeIndex.value = activeIndex.value === null ? 0 : (activeIndex.value + 1) % numDocs.value;
