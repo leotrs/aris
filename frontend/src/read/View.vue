236a28afa4d19b5310be3153686b5e3c4aabe773
Leo Torres <leo.torres@fgsglobal.com>
2025-04-16 15:10:25 +0200

    read view css

--- frontend/src/read/View.vue
+++ frontend/src/read/View.vue
@@ -1,98 +1,109 @@
 <script setup>
- import { ref, watch, onMounted } from 'vue';
- import { useRoute } from 'vue-router';
- import { onKeyUp } from '@vueuse/core';
- import Sidebar from './Sidebar.vue';
- import Topbar from './Topbar.vue';
- import ReadOverlaySettings from './PanelSettings.vue'
+  import { ref, watch, onMounted } from "vue";
+  import { useRoute } from "vue-router";
+  import { onKeyUp } from "@vueuse/core";
+  import Sidebar from "./Sidebar.vue";
+  import Topbar from "./Topbar.vue";
+  import ReadOverlaySettings from "./PanelSettings.vue";
 
- const route = useRoute();
- const htmlContent = ref('');
- const baseURL = `http://localhost:8000/documents/${route.params.doc_id}`;
- onMounted(async () => {
-     try {
-         const response = await fetch(`${baseURL}/html`);
-         if (!response.ok) { throw new Error('Failed to fetch HTML') }
-         htmlContent.value = await response.text();
-     } catch (error) {
-         console.error('Error fetching HTML:', error)
-     }
- });
+  const route = useRoute();
+  const htmlContent = ref("");
+  const baseURL = `http://localhost:8000/documents/${route.params.doc_id}`;
+  onMounted(async () => {
+    try {
+      const response = await fetch(`${baseURL}/content`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch HTML");
+      }
+      htmlContent.value = await response.text();
+    } catch (error) {
+      console.error("Error fetching HTML:", error);
+    }
+  });
 
- const docTitle = ref("");
- const manuscriptRef = ref(null);
- const minimapRef = ref(null);
- const leftColumnFixed = ref(null);
- onMounted(async () => {
-     if (!manuscriptRef.value) return;
+  const docTitle = ref("");
+  const manuscriptRef = ref(null);
+  const minimapRef = ref(null);
+  const leftColumnFixed = ref(null);
+  onMounted(async () => {
+    if (!manuscriptRef.value) return;
 
-     /* Get the title and other info */
-     const response = await fetch(`${baseURL}`);
-     if (!response.ok) { throw new Error('Failed to fetch HTML') }
-     const json = await response.json();
-     docTitle.value = json.title;
+    /* Get the title and other info */
+    const response = await fetch(`${baseURL}`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch HTML");
+    }
+    const json = await response.json();
+    docTitle.value = json.title;
 
-     /* Relocate the minimap */
-     if (leftColumnFixed.value) {
-         const minimap = manuscriptRef.value.querySelector(".float-minimap-wrapper .minimap");
-         if (minimap) {
-             console.log('relocating');
-             minimapRef.value = minimap;
-             minimap.remove();
-             leftColumnFixed.value.append(minimap);
-             minimap.style.visibility = "hidden";
-         }
-     }
- });
+    /* Relocate the minimap */
+    if (leftColumnFixed.value) {
+      const minimap = manuscriptRef.value.querySelector(
+        ".float-minimap-wrapper .minimap"
+      );
+      if (minimap) {
+        console.log("relocating");
+        minimapRef.value = minimap;
+        minimap.remove();
+        leftColumnFixed.value.append(minimap);
+        minimap.style.visibility = "hidden";
+      }
+    }
+  });
 
- const showMinimap = ref(false);
- watch(showMinimap, (newValue, oldValue) => {
-     if (!minimapRef.value) return;
-     console.log(newValue);
-     if (newValue) minimapRef.value.style.visibility = "visible";
-     if (!newValue) minimapRef.value.style.visibility = "hidden";
- })
+  const showMinimap = ref(false);
+  watch(showMinimap, (newValue, oldValue) => {
+    if (!minimapRef.value) return;
+    console.log(newValue);
+    if (newValue) minimapRef.value.style.visibility = "visible";
+    if (!newValue) minimapRef.value.style.visibility = "hidden";
+  });
 
- const showSettings = ref(false);
+  const showSettings = ref(false);
 
- const backgroundColor = ref("var(--surface-page)");
+  const backgroundColor = ref("var(--surface-page)");
 
- onKeyUp (['m', 'M'], (e) => {
-     e.preventDefault();
-     showMinimap.value = !showMinimap.value;
- });
- onKeyUp (['s', 'S'], (e) => {
-     e.preventDefault();
-     showSettings.value = !showSettings.value;
- });
+  onKeyUp(["m", "M"], (e) => {
+    e.preventDefault();
+    showMinimap.value = !showMinimap.value;
+  });
+  onKeyUp(["s", "S"], (e) => {
+    e.preventDefault();
+    showSettings.value = !showSettings.value;
+  });
 </script>
-
 
 <template>
   <div class="read-view">
-
-    <Sidebar
-        v-model:show-minimap="showMinimap"
-        v-model:show-settings="showSettings"
-    />
+    <Sidebar v-model:show-minimap="showMinimap" v-model:show-settings="showSettings" />
 
     <div class="outer-wrapper">
-
       <div class="css-links">
-        <link rel="stylesheet" type="text/css" href="http://localhost:8000/static/rsm.css" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pseudocode@latest/build/pseudocode.min.css">
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="http://localhost:8000/static/rsm.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/pseudocode@latest/build/pseudocode.min.css"
+        />
       </div>
 
       <Topbar :title="docTitle" />
 
       <div class="inner-wrapper">
-
         <div class="left-column" ref="leftColumn"></div>
         <div class="left-column fixed" ref="leftColumnFixed">
           <ReadOverlaySettings
-              v-show="showSettings"
-              @close="showSettings = false"
-              @set-background="(c) => {console.log(c); backgroundColor = c}"
+            v-show="showSettings"
+            @close="showSettings = false"
+            @set-background="
+              (c) => {
+                console.log(c);
+                backgroundColor = c;
+              }
+            "
           />
         </div>
 
@@ -105,125 +116,123 @@
 
         <div class="right-column">right</div>
         <div class="right-column fixed">right fixed</div>
-
       </div>
-
     </div>
   </div>
 </template>
 
-
 <style scoped>
- .read-view {
-     display: flex;
-     width: 100%;
-     background-color: var(--extra-light);
- }
+  .read-view {
+    display: flex;
+    width: 100%;
+    background-color: transparent;
+  }
 
- .outer-wrapper {
-     width: calc(100% - 64px);
-     height: 100%;
-     position: relative;
-     left: 64px;
-     padding-right: 8px;
-     padding-bottom: 8px;
- }
+  .outer-wrapper {
+    width: calc(100% - 64px);
+    height: 100%;
+    position: relative;
+    left: 64px;
+    padding-right: 8px;
+    padding-bottom: 8px;
+  }
 
- .inner-wrapper {
-     display: flex;
-     width: 100%;
-     height: calc(100% - 64px);
-     position: relative;
-     top: 64px;
-     border-radius: 16px;
-     background-color: v-bind("backgroundColor");
-     border-bottom-right-radius: 16px;
-     border-top-right-radius: 16px;
-     overflow-y: auto;
+  .inner-wrapper {
+    display: flex;
+    width: 100%;
+    height: calc(100% - 64px);
+    position: relative;
+    top: 64px;
+    background-color: v-bind("backgroundColor");
+    border-bottom-right-radius: 16px;
+    border-bottom-left-radius: 16px;
+    overflow-y: auto;
 
-     /* These are standard CSS that are not yet supported - switch to them in the future */
-     /* scrollbar-color: var(--light);
+    /* These are standard CSS that are not yet supported - switch to them in the future */
+    /* scrollbar-color: var(--light);
         scrollbar-width: 16px; */
 
-     &::-webkit-scrollbar {
-         width: 16px;
-         background: transparent;
-     }
-     &::-webkit-scrollbar-track {
-         border-bottom-right-radius: 16px;
-         border-top-right-radius: 16px
-     }
-     &::-webkit-scrollbar-thumb {
-         background: var(--gray-200);
-         border-radius: 8px;
-         height: 32px;
-         width: 16px;
-     }
-     &::-webkit-scrollbar-thumb:hover { background: var(--surface-hint) }
- }
+    &::-webkit-scrollbar {
+      width: 16px;
+      background: transparent;
+    }
+    &::-webkit-scrollbar-track {
+      border-bottom-right-radius: 16px;
+      border-top-right-radius: 16px;
+    }
+    &::-webkit-scrollbar-thumb {
+      background: var(--gray-200);
+      border-radius: 8px;
+      height: 32px;
+      width: 16px;
+    }
+    &::-webkit-scrollbar-thumb:hover {
+      background: var(--surface-hint);
+    }
+  }
 
- .left-column, .right-column {
-     flex-basis: 292px;
-     min-width: 120px;
-     background-color: var(--surface-page);
-     padding-inline: 16px;
-     padding-block: 16px;
-     background-color: v-bind("backgroundColor");
-     height: 100%;
- }
+  .left-column,
+  .right-column {
+    flex-basis: 292px;
+    min-width: 120px;
+    background-color: var(--surface-page);
+    padding-inline: 16px;
+    padding-block: 16px;
+    background-color: v-bind("backgroundColor");
+    height: 100%;
+  }
 
- .left-column {
-     border-top-left-radius: 16px;
-     position: relative;
- }
+  .left-column {
+    position: relative;
+  }
 
- .right-column {
-     border-top-right-radius: 16px;
- }
+  .right-column {
+  }
 
- .left-column.fixed, .right-column.fixed {
-     position: fixed;
-     height: calc(100% - 64px);
-     background-color: transparent;
-     top: 64px;
-     overflow-y: auto;
- }
+  .left-column.fixed,
+  .right-column.fixed {
+    position: fixed;
+    height: calc(100% - 64px);
+    background-color: transparent;
+    top: 64px;
+    overflow-y: auto;
+  }
 
- .left-column.fixed {
-     left: 64px;
+  .left-column.fixed {
+    left: 64px;
 
-     & > :deep(.minimap) {
-         position: absolute;
-         width: 48px;
-         top: calc(64px + 40px);
-         height: calc(100% - 152px);
-         left: 0;
-         right: 0;
-         margin: 0 auto;
-     }
+    & > :deep(.minimap) {
+      position: absolute;
+      width: 48px;
+      top: calc(64px + 40px);
+      height: calc(100% - 152px);
+      left: 0;
+      right: 0;
+      margin: 0 auto;
+    }
 
-     & > :deep(.minimap > svg) {
-         height: 100%;
-     }
- }
+    & > :deep(.minimap > svg) {
+      height: 100%;
+    }
+  }
 
- .right-column.fixed {
-     right: 0;
- }
+  .right-column.fixed {
+    right: 0;
+  }
 
- .middle-column {
-     min-width: 576px;
-     max-width: 720px;
-     z-index: 1;
-     overflow-x: visible;
-     height: fit-content;
-     background-color: v-bind("backgroundColor");
- }
+  .middle-column {
+    min-width: 576px;
+    max-width: 720px;
+    z-index: 1;
+    overflow-x: visible;
+    height: fit-content;
+    background-color: v-bind("backgroundColor");
+  }
 
- #footer-logo {
-     display: flex;
-     justify-content: center;
-     padding-top: 48px;
-     padding-bottom: 96px;
- }
+  #footer-logo {
+    display: flex;
+    justify-content: center;
+    padding-top: 48px;
+    padding-bottom: 96px;
+  }
 </style>
