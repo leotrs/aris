bdd03a2aa2b0f2ad79eb88f81e84d5c926b224f8
Leo Torres <leo.torres@fgsglobal.com>
2025-04-16 23:08:31 +0200

    progresss

---
+++ frontend/src/read/SidebarItem.vue
@@ -0,0 +1,14 @@
+<script setup>
+  const props = defineProps({ icon: { type: String, required: true } });
+  const emit = defineEmits(["on", "off"]);
+</script>
+
+<template>
+  <ButtonToggle :icon="icon" @on="$emit('on')" @off="emit('off')">
+    <!-- <SegmentedControl
+      :icons="['LayoutSidebarFilled', 'LayoutNavbarFilled', 'LayoutSidebarRightFilled']"
+    /> -->
+  </ButtonToggle>
+</template>
+
+<style scoped></style>
