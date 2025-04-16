27f2931ade236cd538ca17c492c8843b6e1c435f
Leo Torres <leo.torres@fgsglobal.com>
2025-04-16 12:00:11 +0200

    indedntaiton

--- frontend/src/home/ModalUploadFile.vue
+++ frontend/src/home/ModalUploadFile.vue
@@ -1,46 +1,46 @@
 <script setup>
-import { useTemplateRef } from "vue";
-import useClosable from "@/composables/useClosable.js";
+  import { useTemplateRef } from "vue";
+  import useClosable from "@/composables/useClosable.js";
 
-const emit = defineEmits(["close"]);
-const close = () => emit("close");
+  const emit = defineEmits(["close"]);
+  const close = () => emit("close");
 
-useClosable({ onClose: close });
+  useClosable({ onClose: close });
 
-const fileUpload = useTemplateRef("fileUpload");
-const triggerFileUpload = () => fileUpload.value?.click();
+  const fileUpload = useTemplateRef("fileUpload");
+  const triggerFileUpload = () => fileUpload.value?.click();
 
-const upload = () => {
-  if (fileUpload.value.files.length == 0) return;
-  const file = fileUpload.value.files[0];
+  const upload = () => {
+    if (fileUpload.value.files.length == 0) return;
+    const file = fileUpload.value.files[0];
 
-  const reader = new FileReader();
-  reader.onerror = () => console.log("Error reading the file. Please try again.");
-  reader.onload = () => {
-    const url = "http://localhost:8000/documents/";
-    console.log(reader.result);
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        source: reader.result,
-        owner_id: 1,
-        title: "",
-        abstract: "",
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error("Error:", error));
+    const reader = new FileReader();
+    reader.onerror = () => console.log("Error reading the file. Please try again.");
+    reader.onload = () => {
+      const url = "http://localhost:8000/documents/";
+      console.log(reader.result);
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          source: reader.result,
+          owner_id: 1,
+          title: "",
+          abstract: "",
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.error("Error:", error));
+    };
+    reader.readAsText(file);
   };
-  reader.readAsText(file);
-};
 </script>
 
 <template>
-  <div class="md-wrapper" ref="selfRef">
+  <div class="md-wrapper">
     <div class="md-header">
       <span class="text-h3">Upload New File</span>
       <ButtonClose @close="close" />
@@ -68,46 +68,46 @@ const upload = () => {
 </template>
 
 <style scoped>
-.md-wrapper {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translateX(-50%) translateY(-50%);
-  border: 2px solid var(--border-information);
-  border-radius: 16px;
-  padding: 16px;
-  gap: 16px;
-  display: flex;
-  flex-direction: column;
-  min-width: 500px;
-  background-color: var(--surface-primary);
-}
+  .md-wrapper {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    border: 2px solid var(--border-information);
+    border-radius: 16px;
+    padding: 16px;
+    gap: 16px;
+    display: flex;
+    flex-direction: column;
+    min-width: 500px;
+    background-color: var(--surface-primary);
+  }
 
-.md-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: var(--surface-information);
-  padding-inline: 8px;
-  padding-block: 4px;
-  border-radius: 8px;
-}
+  .md-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: var(--surface-information);
+    padding-inline: 8px;
+    padding-block: 4px;
+    border-radius: 8px;
+  }
 
-.md-content {
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-}
+  .md-content {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
 
-.md-footer {
-  display: flex;
-  gap: 8px;
-  justify-content: flex-end;
-}
+  .md-footer {
+    display: flex;
+    gap: 8px;
+    justify-content: flex-end;
+  }
 
-#file-upload-cta {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-}
+  #file-upload-cta {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
 </style>
