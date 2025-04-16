4cb16b44353a5da3c7b557bb55e2f160b72f98a6
Leo Torres <leo@leotrs.com>
2025-04-16 15:10:25 +0200

    read view css

--- frontend/src/read/Topbar.vue
+++ frontend/src/read/Topbar.vue
@@ -1,70 +1,80 @@
 <script setup>
- const props = defineProps({
-     title: { type: String }
- })
+  const props = defineProps({
+    title: { type: String },
+  });
 </script>
-
 
 <template>
   <div class="tb-wrapper">
-
     <div class="left-column">
       <div class="title">{{ title }}</div>
     </div>
 
-    <div class="middle-column">
-    </div>
+    <div class="middle-column"></div>
 
     <div class="right-column">
-      <Button kind="tertiary" icon="Sparkles"/>
-      <Button kind="tertiary" icon="Share3"/>
-      <Button kind="tertiary" icon="Lifebuoy"/>
-      <Button kind="tertiary"><Avatar name="TER" /></Button>
+      <Button kind="tertiary" icon="Sparkles" />
+      <Button kind="tertiary" icon="Share3" />
+      <Button kind="tertiary" icon="Lifebuoy" />
+      <Button kind="tertiary">
+        <Avatar name="TER" />
+      </Button>
     </div>
   </div>
 </template>
 
-
 <style scoped>
- .tb-wrapper {
-     display: flex;
-     min-height: 64px;
-     max-height: 64px;
-     width: calc(100% - 64px);
-     position: fixed;
-     background-color: var(--extra-light);
-     z-index: 1;
+  .tb-wrapper {
+    --outer-padding: 8px;
+    --sidebar-width: 64px;
 
-     &:has( > .middle-column.empty) {
-         background-color: transparent;
-         & > .left-column, & > .right-column { background-color: var(--surface-page) };
-     }
- }
+    display: flex;
+    height: calc(var(--sidebar-width) - var(--outer-padding));
+    width: calc(100% - var(--sidebar-width) - var(--outer-padding));
+    position: fixed;
+    top: 8px;
+    background-color: transparent;
+    z-index: 1;
 
- .left-column, .right-column {
-     flex-basis: 292px;
-     display: flex;
-     align-items: center;
- }
+    &:has(> .middle-column.empty) {
+      background-color: transparent;
 
- .left-column {
-     padding-inline: 16px;
-     &:has( + .middle-column.empty) {
-     }
- }
+      & > .left-column,
+      & > .right-column {
+        background-color: var(--surface-page);
+      }
+    }
+  }
 
- .right-column {
-     justify-content: flex-end;
-     padding-inline: 8px;
-     gap: 8px;
- }
+  .left-column,
+  .middle-column,
+  .right-column {
+    background-color: var(--surface-page);
+  }
 
- .middle-column {
-     flex-grow: 1;
- }
+  .left-column,
+  .right-column {
+    flex-basis: 292px;
+    display: flex;
+    align-items: center;
+  }
 
- .middle-column.empty {
-     background-color: var(--surface-page);
- }
+  .left-column {
+    border-top-left-radius: 16px;
+    padding-inline: 16px;
+  }
+  .right-column {
+    border-top-right-radius: 16px;
+    justify-content: flex-end;
+    padding-inline: 8px;
+    gap: 8px;
+  }
 
+  .middle-column {
+    flex-grow: 1;
+  }
+
+  .middle-column.empty {
+    background-color: var(--surface-page);
+  }
 </style>
