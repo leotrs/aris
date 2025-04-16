ead030a822004a1c4acd3d62ba3adfd631bc14f3
Leo Torres <leo@leotrs.com>
2025-04-16 14:53:12 +0200

    better display of abstract text

--- frontend/src/common/Abstract.vue
+++ frontend/src/common/Abstract.vue
@@ -1,23 +1,26 @@
 <script setup>
-import { ref, watch, onMounted } from "vue";
-import axios from "axios";
+  import { ref, watch, onMounted } from "vue";
+  import axios from "axios";
 
-const props = defineProps({ doc: { Type: Object, required: true } });
-const abstract = ref("<div>loading abstract...</div>");
+  const props = defineProps({ doc: { Type: Object, required: true } });
+  const abstract = ref("<div>loading abstract...</div>");
 
-const loadAbstract = async () => {
-  try {
-    const response = await axios.get(
-      `http://localhost:8000/documents/${props.doc.id}/content/abstract`,
-    );
-    abstract.value = response.data;
-  } catch (error) {
-    console.error(error);
-    abstract.value = "<div>error when retrieving abstract!</div>";
-  }
-};
-onMounted(loadAbstract);
-watch(() => props.doc, loadAbstract);
+  const truncateText = (text, maxLength = 300) =>
+    text.length > maxLength ? text.substring(0, maxLength) + "..." : text;
+
+  const loadAbstract = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8000/documents/${props.doc.id}/content/abstract?handrails=false`
+      );
+      abstract.value = truncateText(response.data);
+    } catch (error) {
+      console.error(error);
+      abstract.value = "<div>error when retrieving abstract!</div>";
+    }
+  };
+  onMounted(loadAbstract);
+  watch(() => props.doc, loadAbstract);
 </script>
 
 <template>
