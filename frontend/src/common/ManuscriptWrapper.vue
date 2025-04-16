04e6c34ad1f50beda406798298ccffaf446f993c
Leo Torres <leo.torres@fgsglobal.com>
2025-04-16 18:10:11 +0200

    css and indent

--- frontend/src/common/ManuscriptWrapper.vue
+++ frontend/src/common/ManuscriptWrapper.vue
@@ -1,23 +1,23 @@
 <script setup>
-import { ref, watch, onBeforeMount, useTemplateRef, nextTick } from "vue";
+  import { ref, watch, onBeforeMount, useTemplateRef, nextTick } from "vue";
 
-const props = defineProps({ html: { type: String, required: true } });
+  const props = defineProps({ html: { type: String, required: true } });
 
-let onload = ref(null);
-onBeforeMount(async () => {
-  await import("http://localhost:8000/static/jquery-3.6.0.js");
-  await import("http://localhost:8000/static/tooltipster.bundle.js");
-  const module = await import("http://localhost:8000/static/onload.js");
-  onload.value = module.onload;
-});
+  let onload = ref(null);
+  onBeforeMount(async () => {
+    await import("http://localhost:8000/static/jquery-3.6.0.js");
+    await import("http://localhost:8000/static/tooltipster.bundle.js");
+    const module = await import("http://localhost:8000/static/onload.js");
+    onload.value = module.onload;
+  });
 
-const manuscript = useTemplateRef("manuscript-ref");
-const tryExecuteOnload = async () => {
-  if (!onload.value || !manuscript.value || !props.html) return;
-  await nextTick();
-  onload.value(manuscript.value);
-};
-watch([onload, () => manuscript.value, () => props.html], tryExecuteOnload);
+  const manuscript = useTemplateRef("manuscript-ref");
+  const tryExecuteOnload = async () => {
+    if (!onload.value || !manuscript.value || !props.html) return;
+    await nextTick();
+    onload.value(manuscript.value);
+  };
+  watch([onload, () => manuscript.value, () => props.html], tryExecuteOnload);
 </script>
 
 <template>
@@ -38,30 +38,29 @@ watch([onload, () => manuscript.value, () => props.html], tryExecuteOnload);
 </template>
 
 <style>
-/* Overwrite RSM's CSS but be CAREFUL!!! */
-.manuscriptwrapper {
-  /* The background color comes from the user's choice within the settings overlay */
-  background-color: transparent !important;
+  /* Overwrite RSM's CSS but be CAREFUL!!! */
+  .manuscriptwrapper {
+    /* The background color comes from the user's choice within the settings overlay */
+    background-color: transparent !important;
 
-  /* Overwrite size and whitespace */
-  margin: 0 !important;
-  max-width: unset !important;
-  padding-top: 40px !important;
-  padding-bottom: 48px !important;
-  padding-inline: 0px !important;
-  border-radius: 0px !important;
+    /* Overwrite size and whitespace */
+    margin: 0 !important;
+    max-width: unset !important;
+    padding-bottom: 48px !important;
+    padding-inline: 0px !important;
+    border-radius: 0px !important;
 
-  & section.level-1 {
-    margin-block: 0px !important;
+    & section.level-1 {
+      margin-block: 0px !important;
+    }
+
+    /* Patches - for some reason the RSM CSS is broken? */
+    & .hr .hr-border-zone .hr-border-dots .icon.dots {
+      padding-bottom: 0 !important;
+    }
+
+    & .hr .hr-collapse-zone .hr-collapse .icon.collapse {
+      padding-bottom: 0 !important;
+    }
   }
-
-  /* Patches - for some reason the RSM CSS is broken? */
-  & .hr .hr-border-zone .hr-border-dots .icon.dots {
-    padding-bottom: 0 !important;
-  }
-
-  & .hr .hr-collapse-zone .hr-collapse .icon.collapse {
-    padding-bottom: 0 !important;
-  }
-}
 </style>
