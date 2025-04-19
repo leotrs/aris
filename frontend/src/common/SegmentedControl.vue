392de20ed2aa554f453184e0e920feed0e7cba68
Leo Torres <leo.torres@fgsglobal.com>
2025-04-19 20:24:09 +0200

    improvements

--- frontend/src/common/SegmentedControl.vue
+++ frontend/src/common/SegmentedControl.vue
@@ -1,30 +1,48 @@
 <script setup>
+  import { computed, onMounted } from "vue";
   import * as Icons from "@tabler/icons-vue";
-  import { ref, onMounted } from "vue";
 
   const props = defineProps({
-    icons: Array,
+    icons: { type: Array, default: null },
+    labels: { type: Array, default: null },
     defaultActive: { type: Number, default: -1 },
   });
-  const active = defineModel({ type: Number, default: 0 });
-  active.value = props.defaultActive;
 
-  const click = (idx) => {
-    active.value = idx;
-  };
+  const numItems = computed(() => {
+    if (props.icons === null && props.labels === null) {
+      console.error("Must provide one of icons or labels");
+      return 0;
+    } else if (props.icons === null && props.labels !== null) {
+      return props.labels.length;
+    } else if (props.icons !== null && props.labels === null) {
+      return props.icons.length;
+    } else if (props.icons !== null && props.labels !== null) {
+      if (props.icons.length != props.labels.length) {
+        console.error("Lengths of icons and labels must match");
+        return 0;
+      } else {
+        return props.icons.length;
+      }
+    }
+  });
+
+  const active = defineModel({ type: Number, default: -1 });
+  onMounted(() => (active.value = props.defaultActive));
+  const click = (idx) => (active.value = idx);
 </script>
 
 <template>
   <div class="sc-wrapper">
-    <span
-      v-for="(icon, idx) in icons"
-      :key="idx"
-      class="sc-item"
-      :class="['sc-btn', { active: idx == active }]"
-      @click="click(idx)"
-    >
-      <component :is="Icons['Icon' + icon]" class="sc-icon" />
-    </span>
+    <template v-for="idx in numItems">
+      <span
+        class="sc-item"
+        :class="['sc-btn', { active: idx - 1 == active }]"
+        @click="click(idx - 1)"
+      >
+        <component :is="Icons['Icon' + icons[idx - 1]]" v-if="icons" class="sc-icon" />
+        <span v-if="labels" class="sc-label text-default">{{ labels[idx - 1] }}</span>
+      </span>
+    </template>
   </div>
 </template>
 
