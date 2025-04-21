a7a362c1dbf7da8989d2b81e50c195d9a3e9a9f6
Leo Torres <leo.torres@fgsglobal.com>
2025-04-21 22:44:07 +0200

    woops

---
+++ frontend/src/read/PanelTags.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import { watch } from "vue";
+  const props = defineProps({ doc: { type: Object, default: () => {} } });
+</script>
+
+<template>
+  <TagRow v-model="doc.tags" :doc-id="doc.id" />
+</template>
+
+<style scoped></style>
