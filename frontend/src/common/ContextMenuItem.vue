8f5ec9b0d30e0a41b4b3f16ff59ed98979f02b38
Leo Torres <leo@leotrs.com>
2025-04-21 20:59:20 +0200

    random fixes

--- frontend/src/common/ContextMenuItem.vue
+++ frontend/src/common/ContextMenuItem.vue
@@ -15,68 +15,31 @@
 </template>
 
 <style scoped>
-  .dots {
-    position: relative;
+  .item {
     display: flex;
-    color: var(--extra-dark);
-    stroke-width: 2px;
-    border-radius: 8px;
-    padding-inline: 6px;
-    padding-block: 7px;
-
-    & > svg {
-      width: 4px;
-      height: 18px;
-      margin: 0;
-    }
+    align-items: center;
+    padding-left: 10px;
+    padding-right: 16px;
+    padding-block: 0px;
+    gap: 10px;
+    transition: var(--transition-bg-color);
 
     &:hover {
-      background-color: var(--surface-hint);
+      background-color: var(--surface-hover);
     }
   }
 
-  .menu {
-    position: absolute;
-    right: 0;
-    top: 0;
-    transform: translateX(-16px) translateY(-8px);
-    background-color: var(--surface-primary);
-    padding-block: 8px;
-    border-radius: 16px;
-    box-shadow: var(--shadow-strong), var(--shadow-soft);
-
-    & > *:not(:last-child) {
-      margin-bottom: 8px;
-    }
-    & .item {
-      display: flex;
-      align-items: center;
-      padding-left: 10px;
-      padding-right: 16px;
-      padding-block: 0px;
-      gap: 10px;
-
-      &:hover {
-        background-color: var(--surface-hover);
-        font-weight: var(--weight-medium);
-      }
-      &:hover .tabler-icon {
-        stroke-width: 2px;
-      }
-    }
-
-    & .item .tabler-icon {
-      display: inline-block;
-      stroke-width: 1.2px;
-    }
+  .item > .tabler-icon {
+    stroke-width: 1.75px;
   }
 
-  .menu .item.danger {
+  .item.danger {
     color: var(--text-error);
 
     &:hover {
       background-color: var(--surface-error);
     }
+
     & .tabler-icon {
       color: var(--icon-error);
     }
