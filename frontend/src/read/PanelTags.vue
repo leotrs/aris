0fca935bd78666a3b340d54bedb3ad9b98f1ae4b
Leo Torres <leo@leotrs.com>
2025-04-22 09:23:39 +0200

    css

--- frontend/src/read/PanelTags.vue
+++ frontend/src/read/PanelTags.vue
@@ -4,7 +4,23 @@
 </script>
 
 <template>
-  <TagRow v-model="doc.tags" :doc-id="doc.id" />
+  <div class="wrapper">
+    <span class="text-label">Tags: </span>
+    <div class="tag-row">
+      <TagRow v-model="doc.tags" :doc-id="doc.id" />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .wrapper {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .tag-row {
+    display: flex;
+    align-items: center;
+  }
+</style>
