6762de25215236cd534f15181c87a58b4cc7ddc9
Leo Torres <leo.torres@fgsglobal.com>
2025-04-24 21:17:58 +0200

    nicer names

--- frontend/src/home/DragBorder.vue
+++ frontend/src/home/DragBorder.vue
@@ -6,16 +6,16 @@
     active: { type: Boolean, default: false },
     offset: { type: Number, default: 48 },
   });
-  const model = defineModel({ type: Number });
+  const pos = defineModel({ type: Number });
   const pointerEvents = computed(() => (props.active ? "all" : "none"));
 
   /* Usually, we would just set :style="style" on the handle,
    * however, style has px units and we prefer percentages so
    * the handle is responsive to viewport resizing. */
-  const onDrag = (pos) => (model.value = pos.y);
+  const onDrag = (newPos) => (pos.value = newPos.y);
   const containerRef = useTemplateRef("container-ref");
   const { y } = useDraggable(useTemplateRef("handle-ref"), {
-    initialValue: { x: 0, y: window.innerHeight / 3 },
+    initialValue: { x: 0, y: pos.value },
     preventDefault: true,
     axis: "y",
     containerElement: containerRef,
