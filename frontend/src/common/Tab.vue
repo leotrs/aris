ddbb5139f3426a7a0c72a15ff479813d95f7f01f
Leo Torres <leo.torres@fgsglobal.com>
2025-04-27 13:01:47 +0200

    progress

---
+++ frontend/src/common/Tab.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import {} from "vue";
+  import * as Icons from "@tabler/icons-vue";
+
+  const props = defineProps({
+    label: { type: String, default: "" },
+    icon: { type: String, default: "" },
+  });
+</script>
+
+<template>
+  <div class="tab-wrapper">
+    <component :is="Icons['Icon' + icon]" class="tab-icon" />
+    <span class="tab-label">{{ label }}</span>
+  </div>
+</template>
+
+<style>
+  .tab-wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
