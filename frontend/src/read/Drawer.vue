cb7ab52083fdb4799120da8f93dfddff20bcc495
Leo Torres <leo.torres@fgsglobal.com>
2025-04-27 14:46:19 +0200

    initial state and css

--- frontend/src/read/Drawer.vue
+++ frontend/src/read/Drawer.vue
@@ -1,18 +1,22 @@
 <script setup>
-  import { ref, watch, useTemplateRef } from "vue";
+  import { ref, watch, inject, useTemplateRef, onMounted } from "vue";
   import { useKeyboardShortcuts } from "@/composables/useKeyboardShortcuts.js";
 
   const active = defineModel({ type: Boolean, default: false });
+  const doc = inject("doc");
 
   /* State */
   const tabsRef = useTemplateRef("tabs-ref");
-  const activeTabIndex = ref(null);
-  watch(activeTabIndex, () => {
+  const activeTabIndex = ref(0);
+
+  const updatePages = () => {
+    if (!tabsRef.value || !tabsRef.value.$el) return;
     const pages = tabsRef.value.$el.querySelectorAll(".tab-page-wrapper");
     pages.forEach((page) => (page.style.display = "none"));
-    if (activeTabIndex.value === null) return;
     pages[activeTabIndex.value].style.display = "block";
-  });
+  };
+  watch(activeTabIndex, () => updatePages(), { immediate: true });
+  onMounted(() => updatePages());
 
   /* Keys */
   useKeyboardShortcuts({
@@ -31,14 +35,14 @@
       <Tabs
         ref="tabs-ref"
         v-model="activeTabIndex"
-        :labels="['activity', 'history', 'settings', 'citation', 'tags']"
-        :icons="['Bolt', 'Clock', 'FileSettings', 'Quote', 'Tag']"
+        :labels="['activity', 'history', 'citation', 'tags', 'settings']"
+        :icons="['Bolt', 'Clock', 'Quote', 'Tag', 'FileSettings']"
       >
         <TabPage>activity</TabPage>
         <TabPage>history</TabPage>
-        <TabPage>settings</TabPage>
+        <TabPage><TagRow v-model="doc.tags" :doc-id="doc.id" /></TabPage>
         <TabPage>citation</TabPage>
-        <TabPage>tags</TabPage>
+        <TabPage>settings</TabPage>
       </Tabs>
     </div>
   </div>
@@ -47,6 +51,8 @@
 <style>
   .d-wrapper {
     --transition-duration: 0.3s;
+    --tab-width: 64px;
+    --padding: 8px;
 
     position: fixed;
     right: calc(var(--scrollbar-size));
@@ -60,12 +66,12 @@
     border-top-left-radius: 16px;
     border-bottom-left-radius: 16px;
     background-color: var(--surface-primary);
-    padding: 8px;
+    padding: var(--padding);
     width: 0;
     transition: width var(--transition-duration) ease;
   }
 
   .drawer.active {
-    width: 400px;
+    width: calc(64px * 5 + + 4px * 4 + 2 * var(--padding));
   }
 </style>
