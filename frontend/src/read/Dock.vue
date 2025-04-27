95c24a2b9da249b091e40db4fc5403687a8550a9
Leo Torres <leo@leotrs.com>
2025-04-27 12:33:27 +0200

    css

--- frontend/src/read/Dock.vue
+++ frontend/src/read/Dock.vue
@@ -1,16 +1,14 @@
 <script setup>
-  import { ref, onMounted, inject } from "vue";
-
-  const props = defineProps({});
+  import {} from "vue";
 </script>
 
 <template>
-  <div class="drawer">
+  <div class="dock">
     <slot />
   </div>
 </template>
 
 <style scoped>
-  .drawer {
+  .dock {
   }
 </style>
