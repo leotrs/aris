e3b5203c825ff0e6d2b968e93d657b991131e51d
Leo Torres <leo.torres@fgsglobal.com>
2025-04-27 11:45:52 +0200

    drawer

---
+++ frontend/src/read/Drawer.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import {} from "vue";
+</script>
+
+<template>
+  <div class="d-wrapper">
+    <Button kind="tertiary" icon="LayoutSidebarRightExpand" />
+  </div>
+</template>
+
+<style>
+  .d-wrapper {
+    position: fixed;
+    right: calc(var(--outer-padding) + var(--scrollbar-size));
+  }
+
+  .d-wrapper button {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+</style>
