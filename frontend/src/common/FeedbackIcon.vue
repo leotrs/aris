4beacc50cf30e9e348832bb264bcd72067d20089
Leo Torres <leo@leotrs.com>
2025-05-04 13:39:51 +0200

    icons

--- frontend/src/common/FeedbackIcon.vue
+++ frontend/src/common/FeedbackIcon.vue
@@ -3,7 +3,18 @@
 </script>
 
 <template>
-  <div>hello</div>
+  <div class="feedback">
+    <ContextMenu ref="menu-ref" icon="MoodPlus" placement="left-start">
+      <ContextMenuItem icon="BookmarkFilled" caption="Bookmark" />
+      <ContextMenuItem icon="StarFilled" caption="Favorite" />
+      <ContextMenuItem icon="HeartFilled" caption="Love it" />
+      <ContextMenuItem icon="CircleCheckFilled" caption="???" />
+      <ContextMenuItem icon="AlertTriangleFilled" caption="Important" />
+      <ContextMenuItem icon="HelpSquareRoundedFilled" caption="Confusing" />
+      <Separator />
+      <ContextMenuItem icon="X" caption="Remove" />
+    </ContextMenu>
+  </div>
 </template>
 
 <style scoped></style>
