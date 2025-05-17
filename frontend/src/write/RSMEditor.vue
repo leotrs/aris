4ea18d8d6b7f8c8596b8b8cfca786c769b4bb7aa
Leo Torres <leo@leotrs.com>
2025-05-17 15:13:39 +0200

    first stab at the editor component

---
+++ frontend/src/write/RSMEditor.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import {} from "vue";
+
+  const props = defineProps({});
+  const file = defineModel({ type: Object, required: true });
+</script>
+
+<template>
+  <div class="editor">
+    <div class="source">this is the content</div>
+  </div>
+</template>
+
+<style scoped>
+  .editor {
+    border: var(--border-extrathin) solid var(--border-primary);
+    min-height: 300px;
+    box-shadow: inset var(--shadow-strong);
+    border-radius: 16px;
+    padding: 16px;
+    cursor: text;
+    margin-inline: 8px;
+  }
+</style>
