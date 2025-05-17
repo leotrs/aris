33c5934a91c96753e206dc4a2b203fc015b25b93
Leo Torres <leo@leotrs.com>
2025-05-17 19:37:50 +0200

    css

--- frontend/src/write/RSMEditor.vue
+++ frontend/src/write/RSMEditor.vue
@@ -15,18 +15,21 @@
 
 <style scoped>
   pre.editor {
-    overflow: auto;
     font-family: "Source Code Pro", monospace;
     font-size: 14px;
     height: 100%;
-    resize: none;
-    border: none;
-    padding: 16px;
-    cursor: text;
-    margin-inline: 16px 8px;
-    box-shadow: inset var(--shadow-strong);
-    border-radius: 8px;
-    border: var(--border-extrathin) solid var(--border-primary);
     min-height: 300px;
+    overflow: auto;
+    padding: 16px;
+    margin-inline: 16px 8px;
+    border: var(--border-extrathin) solid var(--border-primary);
+    border-radius: 8px;
+    box-shadow: inset var(--shadow-strong);
+    resize: none;
+    cursor: text;
+  }
+
+  pre.editor:focus {
+    outline-color: var(--border-action);
   }
 </style>
