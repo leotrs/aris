e8dfcd8d4fb0e2766aae339450eef5c630e78642
Leo Torres <leo@leotrs.com>
2025-05-19 15:24:33 +0200

    create a toolbar for the editor and corresponding css

--- frontend/src/read/RSMEditor.vue
+++ frontend/src/read/RSMEditor.vue
@@ -17,40 +17,64 @@
 </script>
 
 <template>
-  <textarea
-    ref="editor-ref"
-    class="editor"
-    :value="file.source"
-    spellcheck="false"
-    autocomplete="off"
-    autocorrect="off"
-    autocapitalize="off"
-    @input="onInput"
-  ></textarea>
+  <div class="editor-wrapper">
+    <div class="toolbar">tools go here</div>
+    <textarea
+      ref="editor-ref"
+      class="editor"
+      :value="file.source"
+      spellcheck="false"
+      autocomplete="off"
+      autocorrect="off"
+      autocapitalize="off"
+      @input="onInput"
+    ></textarea>
+  </div>
 </template>
 
 <style scoped>
-  textarea.editor {
-    position: relative;
+  .editor-wrapper {
+    --toolbar-height: 32px;
+
     height: calc(100% - 16px);
-    width: 100%;
-    font-family: "Source Code Pro", monospace;
-    font-size: 14px;
-    padding: 8px;
     margin-inline: 16px;
     border: var(--border-extrathin) solid var(--border-primary);
     border-radius: 8px;
     box-shadow: inset var(--shadow-strong);
+    background-color: var(--surface-page);
+  }
+
+  .editor-wrapper:has(> textarea.editor:focus) {
+    border-color: var(--border-action);
+  }
+
+  .toolbar {
+    height: var(--toolbar-height);
+    border-radius: 8px 8px 0 0;
+    padding: 8px;
+    background-color: var(--surface-hover);
+    box-shadow: var(--shadow-strong);
+  }
+
+  textarea.editor {
+    height: calc(100% - var(--toolbar-height));
+    width: 100%;
+    font-family: "Source Code Pro", monospace;
+    font-size: 14px;
+    padding: 8px;
+    border: none;
+    border-radius: 8px;
     resize: none;
     cursor: text;
     overflow-y: auto;
     white-space: pre-wrap;
     word-break: break-word;
-    background-color: var(--surface-page);
+    background-color: transparent;
     color: var(--text-primary);
+    border-radius: 0 0 16px 16px;
   }
 
   textarea.editor:focus {
-    outline-color: var(--border-action);
+    outline: none;
   }
 </style>
