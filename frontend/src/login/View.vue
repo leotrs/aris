b931b0004679679e3072edf5b19e18fd04ab0c8f
Leo Torres <leo@leotrs.com>
2025-05-23 22:09:15 +0200

    first commit for login view

---
+++ frontend/src/login/View.vue
@@ -0,0 +1,77 @@
+<script setup>
+  import {} from "vue";
+  import { useRouter } from "vue-router";
+
+  const router = useRouter();
+
+  const onLogin = () => {
+    router.push("/");
+  };
+</script>
+
+<template>
+  <div class="view">
+    <div class="left">
+      <div class="logo"></div>
+      <div class="tagline">
+        <p>Scientific publishing.</p>
+        <p>Web-native. Human-first</p>
+      </div>
+    </div>
+    <div class="right">
+      <div class="wrapper">
+        <div class="top">
+          <label>Email</label>
+          <input type="text" />
+          <label>Password</label>
+          <input type="password" />
+          <div class="forgot"><p>Forgot password?</p></div>
+        </div>
+        <div class="bottom">
+          <Button kind="primary" text="Login" @click="onLogin" />
+          <Button kind="secondary" text="Register" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .view {
+    --transition-duration: 0.3s;
+
+    display: flex;
+    flex-grow: 2;
+    height: 100%;
+    width: 100%;
+  }
+
+  .left {
+    background-color: var(--information-50);
+    height: 100%;
+    width: 50%;
+  }
+
+  .right {
+    background-color: var(--surface-primary);
+    height: 100%;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 48px;
+  }
+
+  .right .wrapper {
+    width: 60%;
+  }
+
+  .right .wrapper .top {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .right .bottom button {
+    width: 100%;
+  }
+</style>
