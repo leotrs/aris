3fc91bbefab9a0077f2bfda8f6fdef524a62922d
Leo Torres <leo.torres@fgsglobal.com>
2025-05-23 22:09:15 +0200

    first commit for login view

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "./home/View.vue";
-import ReadView from "./read/View.vue";
+import MainView from "./main/View.vue";
+import LoginView from "./login/View.vue";
 
 const routes = [
   { path: "/", component: HomeView },
-  { path: "/:file_id/read", component: ReadView },
+  { path: "/:file_id/read", component: MainView },
+  { path: "/login", component: LoginView },
 ];
 
 const router = createRouter({
