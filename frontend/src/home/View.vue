a84bfdb3400e7721fa1037eb907401ef4f554c25
Leo Torres <leo.torres@fgsglobal.com>
2025-05-24 11:46:59 +0200

    expose user menu toggle and add shortcut

--- frontend/src/home/View.vue
+++ frontend/src/home/View.vue
@@ -2,6 +2,7 @@
   import { ref, inject, computed, watch, useTemplateRef } from "vue";
   import { useRouter } from "vue-router";
   import { useElementSize } from "@vueuse/core";
+  import { useKeyboardShortcuts } from "@/composables/useKeyboardShortcuts.js";
   import Sidebar from "./Sidebar.vue";
   import FilesPane from "./FilesPane.vue";
   import PreviewPane from "./PreviewPane.vue";
@@ -52,7 +53,11 @@
 
   /* Since the gap between panes is 8px but the outer padding is 16px, when the Preview
    * pane is inactive, its top edge peeks out by 8px. This hides it. */
-  const previewTop = computed(() => (fileStore.selectedFile.value?.id ? "0" : "8px"));
+  const previewTop = computed(() => (fileStore.value.selectedFile.value?.id ? "0" : "8px"));
+
+  // Keys
+  const userMenuRef = useTemplateRef("user-menu");
+  useKeyboardShortcuts({ u: () => userMenuRef.value.toggle() }, true);
 </script>
 
 <template>
@@ -61,7 +66,7 @@
 
     <div class="menus">
       <Button kind="tertiary" icon="Bell" />
-      <UserMenu />
+      <UserMenu ref="user-menu" />
     </div>
 
     <div ref="panes-ref" class="panes">
