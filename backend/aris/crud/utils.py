a8ab02862b4f06bb0734360b5c21e513b0349361
Leo Torres <leo.torres@fgsglobal.com>
2025-05-24 14:11:43 +0200

    update routes and rename variable

--- backend/aris/crud/utils.py
+++ backend/aris/crud/utils.py
@@ -6,19 +6,19 @@ from bs4 import BeautifulSoup
 from ..models import File
 
 
-async def extract_title(doc: File) -> str:
-    if doc is None:
+async def extract_title(file: File) -> str:
+    if file is None:
         return ""
-    if doc.title:
-        return doc.title
+    if file.title:
+        return file.title
 
-    app = rsm.app.ParserApp(plain=doc.source)
+    app = rsm.app.ParserApp(plain=file.source)
     await asyncio.to_thread(app.run)
     return app.transformer.tree.title
 
 
-async def extract_section(doc: File, section_name: str, handrails: bool = True) -> str:
-    app = rsm.app.ProcessorApp(plain=doc.source, handrails=handrails)
+async def extract_section(file: File, section_name: str, handrails: bool = True) -> str:
+    app = rsm.app.ProcessorApp(plain=file.source, handrails=handrails)
     await asyncio.to_thread(app.run)
     html = app.translator.body
 
