bb7cb0ceaf8a2ff900cc67dedae20cc354cce505
Leo Torres <leo@leotrs.com>
2025-05-29 19:18:55 +0200

    create Section and populate account view more

---
+++ frontend/src/common/Section.vue
@@ -0,0 +1,32 @@
+<script setup>
+  import {} from "vue";
+</script>
+
+<template>
+  <div class="section">
+    <div class="title text-h5">
+      <slot name="title" />
+    </div>
+    <slot name="content" />
+    <slot name="footer" />
+  </div>
+</template>
+
+<style scoped>
+  .section {
+    margin-bottom: 32px;
+  }
+
+  .section.danger {
+    border: var(--border-thin) solid var(--border-error);
+    border-radius: 8px;
+  }
+
+  .section.danger > * {
+    padding: 4px;
+  }
+
+  .section.danger .title {
+    background-color: var(--error-50);
+  }
+</style>
--- frontend/src/user/View.vue
+++ frontend/src/user/View.vue
@@ -1,14 +1,56 @@
 <script setup>
   import { inject } from "vue";
+  import { IconUserCircle } from "@tabler/icons-vue";
 
   const user = inject("user");
-  const mobileMode = inject("mobileMode");
 </script>
 
 <template>
   <HomeLayout>
     <Pane>
-      {{ user }}
+      <Header class="text-h4">
+        <IconUserCircle />
+        <span class="title">Account</span>
+      </Header>
+      <div class="main">
+        <div class="left">
+          <Section>
+            <template #title>Profile</template>
+            <template #content>
+              <div>picture</div>
+              <div>{{ user.name }}</div>
+              <div>{{ user.email }}</div>
+              <div>affiliation</div>
+            </template>
+          </Section>
+          <Section>
+            <template #title>Password</template>
+            <template #content>
+              <div>current pwd</div>
+              <div>new pwd</div>
+              <div>confirm</div>
+            </template>
+          </Section>
+          <Section class="danger">
+            <template #title>Danger Zone</template>
+            <template #content>
+              <div>
+                <p>
+                  If you wish to delete your account, make sure you back up your files and data
+                  first.
+                </p>
+                <p>This is an irreversible action.</p>
+              </div>
+            </template>
+          </Section>
+        </div>
+        <div class="right">
+          <Button :class="logout" kind="secondary" icon="Logout" text="log out" />
+          <Button kind="tertiary" size="sm" icon="BrandGit" text="Contribute" />
+          <Button kind="tertiary" size="sm" icon="Lifebuoy" text="Help" />
+          <Button kind="tertiary" size="sm" icon="Heart" text="Donate" />
+        </div>
+      </div>
     </Pane>
   </HomeLayout>
 </template>
@@ -21,5 +63,36 @@
     height: 100%;
     will-change: padding;
     transition: padding var(--transition-duration) ease;
+  }
+
+  .pane-header {
+    height: 48px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
+  .pane-header > .title {
+    margin-left: 4px;
+  }
+
+  .main {
+    display: flex;
+  }
+
+  .main > .left {
+    flex: 4;
+  }
+
+  .main > .right {
+    flex: 1;
+  }
+
+  .main > .right > button.logout {
+  }
+
+  .main > .right > button {
+    width: 100%;
+    margin-bottom: 8px;
   }
 </style>
