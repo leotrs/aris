75362e75b3ec4cf71634666f6993a9cda25ef97f
Leo Torres <leo.torres@fgsglobal.com>
2025-05-31 18:11:38 +0200

    show tags again

--- frontend/src/common/TagRow.vue
+++ frontend/src/common/TagRow.vue
@@ -1,24 +1,24 @@
 <script setup>
-  import { ref } from "vue";
+  import { ref, watchEffect } from "vue";
   const props = defineProps({
     file: { type: Object, required: true },
   });
-  const tags = defineModel({ type: Array });
 
-  const selectedTags = ref([...(tags.value || [])]);
+  const selectedTags = ref([]);
+  watchEffect(() => {
+    selectedTags.value = [...(props.file.tags || [])];
+  });
 </script>
 
 <template>
   <div class="tag-row">
-    <!-- wrap the last element in a div together with MultiSelectTags so if the row needs to wrap, the MultiSelect is never alone on the second row -->
-    <template v-if="tags && tags.length > 1">
-      <Tag v-for="tag in tags.slice(0, -1)" :key="tag" :tag="tag" :active="true" />
-    </template>
+    <Tag v-for="tag in selectedTags.slice(0, -1)" :key="tag" :tag="tag" :active="true" />
+    <!-- the last tag is wrapped in a div together with MultiSelectTags so if the row needs to wrap, the MultiSelect is never alone on the second row -->
     <div class="nowrap">
       <Tag
-        v-if="tags && tags.length > 0"
-        :key="tags.at(-1) ?? {}"
-        :tag="tags.at(-1) ?? {}"
+        v-if="selectedTags.length > 0"
+        :key="selectedTags.at(-1) ?? {}"
+        :tag="selectedTags.at(-1) ?? {}"
         :active="true"
       />
       <MultiSelectTags v-model="selectedTags" :file="file" />
