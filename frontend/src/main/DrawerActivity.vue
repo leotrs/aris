e0810e39600795de78eaf7ffcfc6c7d185a36ca1
Leo Torres <leo@leotrs.com>
2025-06-06 21:52:13 +0200

    new drawer

---
+++ frontend/src/main/DrawerActivity.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import {} from "vue";
+</script>
+
+<template>
+  <Pane>
+    <template #header>Activity</template>
+    <Section>
+      <template #title>Latest Changes</template>
+      <template #content>
+        <p>As a fun timeline</p>
+      </template>
+    </Section>
+    <Section>
+      <template #title>Version History</template>
+      <template #content>
+        <p>List with quick actions: view, revert, compare, delete</p>
+      </template>
+    </Section>
+  </Pane>
+</template>
+
+<style scoped></style>
