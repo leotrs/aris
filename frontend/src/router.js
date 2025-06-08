ee173d6e78201c921cd26ccc3b4a979ad54ad5b6
Leo Torres <leo.torres@fgsglobal.com>
2025-06-08 22:12:44 +0200

    more

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -29,7 +29,7 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   const publicPages = ["/login", "/register"];
   const authRequired = !publicPages.includes(to.path);
-  const token = localStorage.getItem("accessToken");
+  const token = localStorage.getItem("accessToken")?.trim();
   return (authRequired && !token) ? next("/login") : next();
 });
 
