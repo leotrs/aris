7a030246696c4ee3536cc25c436563d2664666d9
Leo Torres <leo@leotrs.com>
2025-06-09 21:32:27 +0200

    forgot to add this lol see prev commit

---
+++ frontend/src/common/IconTherefore.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import {} from "vue";
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    :class="$attrs.class"
+    :style="$attrs.style"
+    role="img"
+    aria-label="Therefore symbol"
+  >
+    <rect x="10.25" y="4.75" width="3.5" height="3.5" rx="1" ry="1" fill="currentColor" />
+    <rect x="5.25" y="14.75" width="3.5" height="3.5" rx="1" ry="1" fill="currentColor" />
+    <rect x="14.75" y="14.75" width="3.5" height="3.5" rx="1" ry="1" fill="currentColor" />
+  </svg>
+</template>
