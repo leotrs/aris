189646b62997b756adab4cebda508e95e0f43f8f
Leo Torres <leo@leotrs.com>
2025-06-16 10:33:59 +0200

    new models

--- backend/tests/test_models/test_models.py
+++ backend/tests/test_models/test_models.py
@@ -1,8 +1,17 @@
 """Unit tests for model-level logic in aris.models.models."""
 
 import random
-import pytest
-from aris.models.models import AvatarColor, FileStatus, FileSettings
+from aris.models.models import (
+    AvatarColor,
+    FileStatus,
+    FileSettings,
+    File,
+    User,
+    Annotation,
+    AnnotationType,
+    AnnotationMessage,
+    Note,
+)
 
 
 def test_avatar_color_values():
@@ -49,3 +58,73 @@ def test_file_settings_column_defaults():
     assert table.columns["margin_width"].default.arg == "16px"
     # columns count
     assert table.columns["columns"].default.arg == 1
+
+
+def test_annotation_creation(db_session):
+    """Test basic creation of an Annotation."""
+    file = File()
+    db_session.add(file)
+    db_session.commit()
+
+    annotation = Annotation(file_id=file.id)
+    db_session.add(annotation)
+    db_session.commit()
+
+    assert annotation.id is not None
+    assert annotation.file_id == file.id
+    assert annotation.created_at is not None
+
+
+def test_annotation_message_creation(db_session):
+    """Test creating an AnnotationMessage and linking it to Annotation and User."""
+    file = File()
+    user = User()
+    db_session.add_all([file, user])
+    db_session.commit()
+
+    annotation = Annotation(file_id=file.id)
+    db_session.add(annotation)
+    db_session.commit()
+
+    msg = AnnotationMessage(
+        annotation_id=annotation.id,
+        owner_id=user.id,
+        type=AnnotationType.NOTE,
+        content="Sample note",
+    )
+    db_session.add(msg)
+    db_session.commit()
+
+    assert msg.id is not None
+    assert msg.content == "Sample note"
+    assert msg.annotation_id == annotation.id
+    assert msg.owner_id == user.id
+
+
+def test_note_creation(db_session):
+    """Test that a Note can be created and linked to an AnnotationMessage."""
+    file = File()
+    user = User()
+    db_session.add_all([file, user])
+    db_session.commit()
+
+    annotation = Annotation(file_id=file.id)
+    db_session.add(annotation)
+    db_session.commit()
+
+    msg = AnnotationMessage(
+        annotation_id=annotation.id,
+        owner_id=user.id,
+        type=AnnotationType.NOTE,
+        content="Top-level note",
+    )
+    db_session.add(msg)
+    db_session.commit()
+
+    note = Note(message_id=msg.id)
+    db_session.add(note)
+    db_session.commit()
+
+    assert note.message_id == msg.id
+    assert note.message.id == msg.id
+    assert note.message.content == "Top-level note"
