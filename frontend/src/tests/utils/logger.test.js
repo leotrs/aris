c5f767255e32c595f517c47316546bb6a5103ca6
Leo Torres <leo.torres@fgsglobal.com>
2025-06-27 08:46:09 +0200

    fix some tests and lint

--- frontend/src/tests/utils/logger.test.js
+++ frontend/src/tests/utils/logger.test.js
@@ -5,6 +5,9 @@ describe("Logger", () => {
   let consoleSpy;
 
   beforeEach(() => {
+    // Set environment to development to ensure DEBUG level logging
+    vi.stubEnv('MODE', 'development');
+    
     consoleSpy = {
       debug: vi.spyOn(console, "debug").mockImplementation(() => {}),
       info: vi.spyOn(console, "info").mockImplementation(() => {}),
